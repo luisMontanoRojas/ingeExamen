b20f037aeb6caded494412ddd0f011639c6565be
luisMontanoRojas <lushio142356@gmail.com>
2020-06-16 09:51:30 -0400

    Makked cucumbre test

---
+++ features/step_definitions/home_steps.rb
@@ -0,0 +1,15 @@
+Given('visito la pagina home') do
+    pending ""
+  end
+  
+  Given('ingreso el monto {string} en el campo {string}') do |string, string2|
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  When('preciono el boton {string}') do |string|
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  Then('deberia ver un mensaje El balance de tu cuenta es:{string}') do |string|
+    pending # Write code here that turns the phrase above into concrete actions
+  end
